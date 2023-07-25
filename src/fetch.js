692ee983106f4ff6ae4440a538cfeea83e9fc7f0
scuba <scuba75@gmail.com>
2023-07-25 12:02:52 +0000

    update

--- src/fetch.js
+++ src/fetch.js
@@ -1,31 +1,26 @@
 'use strict'
+const path = require('path')
 const log = require('logger')
 const fetch = require('node-fetch')
+const S3_API_URI = process.env.S3_API_URI
+const S3_BUCKET = process.env.S3_DATA_BUCKET || 'gamedata'
 const parseResponse = async(res)=>{
   try{
     if(res?.status?.toString().startsWith(4)) throw('Fetch Error')
     if(!res?.status?.toString().startsWith('2')) return
-    let body
-    if(res.headers?.get('Content-Type')?.includes('application/json')) body = await res.json()
-    if(res.headers?.get('Content-Type')?.includes('text/plain')) body = await res.text()
-    if(res.headers?.get('Content-Disposition')?.includes('filename')){
-      body = await res.arrayBuffer()
-      body = Buffer.from(body)
-    }
-    return body
+    if(res.headers?.get('Content-Type')?.includes('application/json')) return await res.json()
   }catch(e){
     throw(e)
   }
 }
-module.exports = async(uri, method = 'GET', body, headers)=>{
+module.exports = async(file)=>{
   try{
-    let payload = { method: method, compress: true, timeout: 60000 }
-    if(body) payload.body = JSON.stringify(body)
-    if(headers) payload.headers = headers
-    const res = await fetch(uri, payload)
+    if(!file || !S3_API_URI) return
+    let payload = { method: 'GET', compress: true, timeout: 60000 }
+    let res = await fetch(path.join(S3_API_URI, 'get?Bucket='+S3_BUCKET+'&Key='+file), payload)
     return await parseResponse(res)
   }catch(e){
-    log.info('error getting '+uri)
+    log.info('error getting '+file)
     throw(e);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 'use strict'
 const log = require('logger')
-let logLevel = process.env.LOG_LEVEL || log.Level.INFO;
-log.setLevel(logLevel);
 const path = require('path')
 const fs = require('fs')
 const fetch = require('./fetch')
-const DATA_URL = process.env.GITHUB_REPO_RAW_URL || 'https://raw.githubusercontent.com/swgoh-utils/gamedata/main'
+
 const DATA_PATH = process.env.DATA_PATH || path.join(baseDir, 'data')
 
 const saveFile = async(fileName, fileData)=>{
   try{
-    await fs.writeFileSync(path.join(DATA_PATH, fileName), fileData)
+    await fs.writeFileSync(path.join(DATA_PATH, fileName), JSON.stringify(fileData))
   }catch(e){
     throw(e)
   }
@@ -25,15 +23,14 @@ const checkFileExists = async(fileName)=>{
 }
 const downloadFiles = async()=>{
   try{
-    let versions = await fetch(path.join(DATA_URL, 'versions.json'))
-    if(versions) versions = JSON.parse(versions)
+    let versions = await fetch('versions.json')
     if(versions?.gameVersion && versions?.localeVersion){
       log.info('Downloading files for '+versions.gameVersion+'...')
       for(let i in versions){
         if(versions[i] !== versions.localeVersion && i !== 'gameVersion' && i !== 'assetVersion'){
           let status = await checkFileExists(i)
           if(status) continue;
-          let fileData = await fetch(path.join(DATA_URL, i))
+          let fileData = await fetch(i)
           if(fileData){
             await saveFile(i, fileData)
           }else{
@@ -41,21 +38,21 @@ const downloadFiles = async()=>{
           }
         }
       }
-      let localeFile = await fetch(path.join(DATA_URL, 'Loc_ENG_US.txt.json'))
+      let localeFile = await fetch('Loc_ENG_US.txt.json')
       if(localeFile){
         await saveFile('Loc_ENG_US.txt.json', localeFile)
       }else{
         throw('Error getting Loc_ENG_US.txt.json')
       }
-      let localeKeyMap = await fetch(path.join(DATA_URL, 'Loc_Key_Mapping.txt.json'))
+      let localeKeyMap = await fetch('Loc_Key_Mapping.txt.json')
       if(localeKeyMap){
         await saveFile('Loc_Key_Mapping.txt.json', localeKeyMap)
       }else{
         throw('Error getting Loc_Key_Mapping.txt.json')
       }
-      log.info('Downloaded all files from github..')
+      log.info('Downloaded all files from object storage..')
     }else{
-      throw('error getting version.json from github...')
+      throw('error getting version.json from object storage...')
     }
   }catch(e){
     log.error(e)
